06b5dc5ed0ba1177d9832cdb005f585188901bcc
Marianna Cherniuk <Marianna@Mariannas-MacBook-Air.local>
2019-09-03 20:00:02 -0700

    server almost done

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,6 +4,26 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+
+//PRODUCT BY ID METHODE//
+
+exports.productById = (req, res, next,id ) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product);
+}
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -13,9 +33,29 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
+        // CHECK ALL THE FIELD//   ///verify validation 
+
+        // const { name, description, price, category, quantity, shipping, } = fields
+        
+        // if (!name || !description || !price || !category || !quantity || !shipping) {
+        //     return res.status(400).json({
+        //         error: "All field require"
+        //     });
+            
+        // }
         let product = new Product(fields);
+        console.log(product)
+
+        
 
         if (files.photo) {
+            console.log('files Photo', files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                error: "Image should be less than 1MB size "
+            });
+            }
+            
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
@@ -30,3 +70,70 @@ exports.create = (req, res) => {
         });
     });
 };
+
+//DELETE  PRODUCT METHODE//
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            "message":"product deleted sucessfully"
+        })
+    })
+}
+//UPDATE PRODUCT METHODE //
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+        // CHECK ALL THE FIELD//   ///verify validation 
+
+        // const { name, description, price, category, quantity, shipping, } = fields
+        
+        // if (!name || !description || !price || !category || !quantity || !shipping) {
+        //     return res.status(400).json({
+        //         error: "All field require"
+        //     });
+            
+        // }
+        let product = req.product
+        product = _.extend(product, fields);
+        console.log(product)
+
+        
+
+        if (files.photo) {
+            console.log('files Photo', files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                error: "Image should be less than 1MB size "
+            });
+            }
+            
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+};
+
+
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,17 +3,28 @@ const router = express.Router();
 
 //METHODE CREATE NEW PRODUCT BASE ON SCHEMA//
 
-const { create } = require("../controllers/product");
+const { create, productById, read, remove, update } = require("../controllers/product");
+
 //METHODE SIGN UP AUTH AND ADMIN//
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-//ROUTE POST NEW CATEGORY//
+//ROUTE GET PRODUCT BY ID//
+router.get('/product/:productId', read)
+
+//DELETE PRODUCT//
+router.delete("/product/:productId/:userId", requireSignin, isAuth, isAdmin, remove)
+
+//UPDATE PRODUCT//
+router.put("/product/:productId/:userId", requireSignin, isAuth, isAdmin, update,)
+
+//ROUTE POST NEW PRODUCT//
 
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 
 module.exports = router;
