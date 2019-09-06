540b796ce98e6c0c4bb0fa182dcafb0f5fc98d4a
Marianna Cherniuk <Marianna@Aarons-iPad.localdomain>
2019-09-06 13:38:10 -0700

    new product

--- client/src/component/admin/AddProduct.js
+++ client/src/component/admin/AddProduct.js
@@ -1,19 +1,176 @@
-import React, {useState, useEffect}from 'react';
-import Layout from '../Layout';
-import { isAuthenticated } from '../auth/Methode'
-import {createProduct} from "./ApiAdmin";
+
+import React, { useState, useEffect } from "react";
+import Layout from "../Layout";
+import { isAuthenticated } from "../auth/Methode";
 import { Link } from "react-router-dom";
+import { createProduct,getCategories } from './ApiAdmin';
 
 const AddProduct = () => {
-const {user,token,} = isAuthenticated ()
-    return (
-        <Layout title="Add a Product" description={`Create a new Product ${user.name}!`}>
-            <div>
-                ......
-            </div>
-        </Layout>
+    const [values, setValues] = useState({
+        name: "",
+        description: "",
+        price: "",
+        categories: [],
+        category: "",
+        shipping: "",
+        quantity: "",
+        photo: "",
+        loading: false,
+        error: "",
+        createdProduct: "",
+        redirectToProfile: false,
+        formData: ""
+    });
+
+    const { user, token } = isAuthenticated();
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+
+    //LOAD CATEGORIES AND SET FORM DATA //
+
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setValues({...values ,error:data.error})
+            } else {
+                setValues({...values, categories:data, formData: new FormData ()})
+            }
+        })
+    }
+
+
+
+    useEffect(() => {
+        init()
+    }, []);
+
+    const handleChange = name => event => {
+        const value =
+            name === "photo" ? event.target.files[0] : event.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value });
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({ ...values, error: "", loading: true });
+
+        createProduct(user._id, token, formData).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setValues({
+                    ...values,
+                    name: "",
+                    description: "",
+                    photo: "",
+                    price: "",
+                    quantity: "",
+                    loading: false,
+                    createdProduct: data.name
+                });
+            }
+        });
+    };
+
+    const showError = () => (
+        <div style={{ display: error ? '': 'none' }}>
+            {error}
+
+        </div>
+    )
+    //DOUBLE CHECK THE BCK END //
+
+    const showSucess = () => (
+        <div style={{ display: createdProduct ? '' : 'none' }}>
+            <h2>{`${createdProduct}`} is created</h2>
+        </div>
     )
 
-}
+    const showLoading = () => (
+        loading && (<div>
+            <h2>Loading...</h2>
+        </div>)
+    )
+
+    const newPostForm = () => (
+        <form className="mb-3" onSubmit={clickSubmit}>
+            <h4>Post Photo</h4>
+            <div className="form-group">
+                <label className="btn btn-secondary">
+                    <input
+                        onChange={handleChange("photo")}
+                        type="file"
+                        name="photo"
+                        accept="image/*"
+                    />
+                </label>
+            </div>
+
+            <div>
+                <label>Name</label>
+                <input onChange={handleChange("name")} type="text" value={name}/>
+            </div>
+
+            <div>
+                <label>Description</label>
+                <textarea onChange={handleChange("description")} value={description}/>
+            </div>
+
+            <div>
+                <label>Price</label>
+                <input onChange={handleChange("price")} type="number" value={price}/>
+            </div>
+
+            <div>
+                <label>Category</label>
+                <select onChange={handleChange("category")}>
+                    <option>Please select</option>
+                    {categories && categories.map((c, i) => (<option key={i} value={c._id}>{c.name}</option>))}
+                </select>
+            </div>
+
+            <div>
+                <label>Shipping</label>
+                <select onChange={handleChange("shipping")}>
+                    <option>Please select</option>
+                    <option value="0">No</option>
+                    <option value="1">Yes</option>
+                </select>
+            </div>
+
+            <div>
+                <label>Quantity</label>
+                <input onChange={handleChange("quantity")} type="number" value={quantity}/>
+            </div>
+            <button>Create Product</button>
+        </form>
+    );
+
+    return (
+        <Layout title="Add a new product" description={`Create a new Product ${user.name}!`}>
+            <div>
+                <div>
+                    {showLoading()}
+                    {showSucess()}
+                    {showError()}
+                    {newPostForm()}</div>
+            </div>
+        </Layout>
+    );
+};
 
 export default AddProduct;
+
--- client/src/component/admin/ApiAdmin.js
+++ client/src/component/admin/ApiAdmin.js
@@ -42,4 +42,14 @@ const API = 'http://localhost:8000/api'
     
         })
         
- }
+  }
+ 
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET',
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log(err));
+  }
